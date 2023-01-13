4a8c021450e68af6515ed926bd24b370f831767a
joo-prog <1114nhj@naver.com>
2023-01-13 16:24:17 +0900

    feat: 로그인되어있는지 여부, 글을 등록하는 사람과 로그인한 사람이 같은지 확인
    * /problems로 들어왔을 때 바로 컨트롤러로 보내기 전에 auth middleware를 거쳐 확인
    * 로그인되어있는지
    * 로그인한 유저와 글 등록하고자 하는 유저가 같은지

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import { UnAuthorizedException } from '../error/httpException';
+
+class AuthMiddleware {
+  public isLogined(req: Request, res: Response, next: NextFunction) {
+    const session: any = req.session;
+
+    if (session.userId === undefined) throw new UnAuthorizedException('로그인을 해주세요.');
+    else next();
+  }
+
+  public checkRequestUser(req: Request, res: Response, next: NextFunction) {
+    const session: any = req.session;
+    const sessionUser = session.userId;
+    const requestUser = req.body.userId;
+
+    if (sessionUser === requestUser) next();
+    else throw new UnAuthorizedException('권한이 없습니다.');
+  }
+}
+
+export default AuthMiddleware;
--- backend/src/routes/problem.ts
+++ backend/src/routes/problem.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import ProblemController from '../controllers/problem.controllers';
+import AuthMiddleware from '../middlewares/auth';
 
 const router = express.Router();
 const problmesController = new ProblemController();
+const authMiddleware = new AuthMiddleware();
 
-router.post('/problems', problmesController.createProblem.bind(problmesController));
+router.post(
+  '/',
+  authMiddleware.isLogined,
+  authMiddleware.checkRequestUser,
+  problmesController.createProblem.bind(problmesController)
+);
 
 export default router;
