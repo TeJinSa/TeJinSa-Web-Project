0795e7965ecb27d256127285f6bb43135ee6bf42
iyu88 <iyu88@naver.com>
2023-01-15 03:14:30 +0900

    feat: ProblemList 레이아웃 작성
    ProblemList 내부에 Table 을 위치시키고 디자인을 적용했습니다.

--- frontend/src/components/ProblemList/index.tsx
+++ frontend/src/components/ProblemList/index.tsx
@@ -1,11 +1,67 @@
+import styled from 'styled-components';
+import { AiOutlinePlus } from 'react-icons/ai';
 import UserCommonContainer from '../UserCommonContainer';
-import ProblemListItem from '../ProblemListItem';
+
+const ProblemListWrapper = styled.table`
+  width: 100%;
+`;
+
+const ProblemListTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NewProblemButton = styled.button`
+  display: flex;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  background-color: black;
+  cursor: pointer;
+`;
+
+const ProblemListHeader = styled.thead`
+  background-color: lightGrey;
+  th:first-of-type {
+    border-top-left-radius: 10px;
+    padding-left: 1rem;
+  }
+
+  th:last-of-type {
+    border-top-right-radius: 10px;
+    padding-right: 1rem;
+  }
+`;
+
+const ProblemAttribute = styled.th`
+  padding: 0.75rem 0;
+  text-align: center;
+`;
 
 const ProblemList = () => {
   return (
     <UserCommonContainer>
-      <h1>문제 리스트 목록</h1>
-      <ProblemListItem />
+      <ProblemListTitle>
+        <h2>푼 문제 목록</h2>
+        <NewProblemButton>
+          <AiOutlinePlus size="20" color="white" />
+        </NewProblemButton>
+      </ProblemListTitle>
+      <ProblemListWrapper cellSpacing="0">
+        <ProblemListHeader>
+          <ProblemAttribute>플랫폼</ProblemAttribute>
+          <ProblemAttribute>난이도</ProblemAttribute>
+          <ProblemAttribute>문제 링크</ProblemAttribute>
+          <ProblemAttribute>증빙</ProblemAttribute>
+          <ProblemAttribute>날짜</ProblemAttribute>
+          <ProblemAttribute>{null}</ProblemAttribute>
+        </ProblemListHeader>
+        <tbody />
+      </ProblemListWrapper>
     </UserCommonContainer>
   );
 };
