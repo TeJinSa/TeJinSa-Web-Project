683e261c1ccde6fea58be8f9bf0c41a1d3d2a40a
iyu88 <iyu88@naver.com>
2023-01-15 04:54:08 +0900

    style: ProblemListItem 스타일 적용

--- frontend/src/components/ProblemListItem/index.tsx
+++ frontend/src/components/ProblemListItem/index.tsx
@@ -1,17 +1,50 @@
+import styled from 'styled-components';
 import SolvedProblem from '../../types/solvedProblem';
+
+const ProblemItemWrapper = styled.tr`
+  td:first-of-type {
+    padding-left: 1rem;
+  }
+
+  td:last-of-type {
+    padding-right: 1rem;
+  }
+`;
+
+const ProblemAttribute = styled.td`
+  padding: 0.75rem 0;
+  text-align: center;
+  border-bottom: 1px solid black;
+`;
+
+const ProblemCommonButton = styled.button`
+  background-color: white;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+const ProblemImageButton = styled(ProblemCommonButton)`
+  padding: 0.375rem 1rem;
+  border-radius: 10px;
+  color: white;
+  background-color: #745aa8;
+`;
 
 const ProblemListItem = ({ platform, level, link, image, date }: SolvedProblem) => {
   return (
-    <tr>
-      <td>{platform}</td>
-      <td>{level}</td>
-      <td>{link}</td>
-      <td>
-        <button type="button">사진 보기</button>
-      </td>
-      <td>{date}</td>
-      <td>···</td>
-    </tr>
+    <ProblemItemWrapper>
+      <ProblemAttribute>{platform}</ProblemAttribute>
+      <ProblemAttribute>{level}</ProblemAttribute>
+      <ProblemAttribute>{link}</ProblemAttribute>
+      <ProblemAttribute>
+        <ProblemImageButton type="button">사진 보기</ProblemImageButton>
+      </ProblemAttribute>
+      <ProblemAttribute>{date}</ProblemAttribute>
+      <ProblemAttribute>
+        <ProblemCommonButton type="button">···</ProblemCommonButton>
+      </ProblemAttribute>
+    </ProblemItemWrapper>
   );
 };
 
