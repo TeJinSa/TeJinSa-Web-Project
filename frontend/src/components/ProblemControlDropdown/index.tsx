711236335f968fbe69643b48835c357de6027c45
iyu88 <iyu88@naver.com>
2023-01-15 15:40:13 +0900

    feat: Dropdown 내부 태그 수정 및 이벤트 핸들러 적용
    Dropdown 을 구성하는 스타일 컴포넌트에서 사용한 HTML 태그를 사용 용도에 알맞게 변경했습니다.
    기존에는 최상단 태그를 버튼으로 생각하고 구현했지만, DIV 태그로 변경하고 내부에 위치하는 구체적인 옵션의 태그를 버튼으로 변경했습니다.
    실제로 사용자가 최종 액션을 수행하는 곳에 버튼이 위치하면 좋겠다고 판단했습니다.
    
    DropdowmWrapper 를 클릭하면 내부의 isDisplayed 상태가 변경되도록 했습니다.
    상태값의 이름 또한 boolean 타입에 맞게 is~ 형태로 변경했습니다.
    
    내부의 옵션을 눌렀을 때의 동작은 테스트 이벤트 핸들러를 적용하여 value 속성에 따라 정상적으로 작동하는지 확인했습니다.

--- frontend/src/components/ProblemControlDropdown/index.tsx
+++ frontend/src/components/ProblemControlDropdown/index.tsx
@@ -1,47 +1,52 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 interface DropdownDisplay {
-  display: boolean;
+  isDisplayed: boolean;
 }
 
-const DropdownWrapper = styled.button`
+const DropdownWrapper = styled.div`
   position: relative;
-  border: none;
-  outline: none;
-  color: black;
-  background-color: white;
   cursor: pointer;
   transition: all 0.2s;
 `;
 
 const DropdownSelect = styled.div<DropdownDisplay>`
   position: absolute;
-  width: 3rem;
-  padding: 0.5rem;
-  border-radius: 20px;
-  border: 1px solid lightGrey;
-  background-color: white;
   z-index: 999;
-  display: ${(props) => (props.display ? 'block' : 'none')};
+  display: ${(props) => (props.isDisplayed ? 'block' : 'none')};
 `;
 
-const DropdownOption = styled.div`
+const DropdownOption = styled.button`
+  width: 3.5rem;
   padding: 0.5rem;
+  border: 1px solid lightGrey;
+  background-color: white;
   color: ${(props) => props.color ?? 'black'};
 `;
 
 const ProblemControlDropdown = ({ displayText }: { displayText: string }) => {
-  const [dropdownDisplay, setDropdownDisplay] = useState(false);
+  const [isDisplayed, setIsDisplayed] = useState(false);
 
-  const toggleDropdownDisplay = () => setDropdownDisplay(() => !dropdownDisplay);
+  const toggleDropdownDisplay = () => setIsDisplayed(() => !isDisplayed);
+
+  const testHandleDropdownOption = (e: React.MouseEvent<HTMLDivElement>) => {
+    const clickedOption = e.target as HTMLButtonElement;
+    if (clickedOption.value === 'edit') {
+      window.alert('푼 문제 정보를 수정하는 Modal 이 뜹니다.');
+    } else {
+      window.alert('푼 문제 정보 삭제를 확인하는 Modal 이 뜹니다.');
+    }
+  };
 
   return (
-    <DropdownWrapper type="button" onClick={toggleDropdownDisplay}>
+    <DropdownWrapper onClick={toggleDropdownDisplay}>
       {displayText}
-      <DropdownSelect display={dropdownDisplay}>
-        <DropdownOption>수정</DropdownOption>
-        <DropdownOption color="red">삭제</DropdownOption>
+      <DropdownSelect isDisplayed={isDisplayed} onClick={testHandleDropdownOption}>
+        <DropdownOption value="edit">수정</DropdownOption>
+        <DropdownOption value="delete" color="red">
+          삭제
+        </DropdownOption>
       </DropdownSelect>
     </DropdownWrapper>
   );
