efc206d86dc4a7f434533c464a62fd83bc7dc2e9
caseBread <kgu0515@gmail.com>
2023-01-16 19:42:05 +0900

    save

---
+++ frontend/src/global/global.style.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html,body {
+        margin:0;
+        padding:0;
+        font-size:16px;
+    }
+    body { 
+        background-color: #F3F3F3;
+    }
+`;
+
+export default GlobalStyle;
