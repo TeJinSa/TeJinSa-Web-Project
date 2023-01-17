c04a522a97c6d2c3caac289282d3abeefd076f05
caseBread <kgu0515@gmail.com>
2023-01-17 16:34:57 +0900

    chore : 변수명 수정
    data변수명을 더 명확하게 수정

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import { BASE_URL } from '../utils/constants/url';
 
+interface PostLoginResponse {
+  isSuccess: boolean;
+  code: 200;
+  message: '성공';
+  userData: {
+    id: 1;
+    userId: '{github Id}';
+  };
+}
+
 export const postLogin = async ({ githubCode }: { githubCode: string }) => {
-  const { data } = await axios.post(`${BASE_URL}/users/login`, { githubCode });
-  return data;
+  const { userData }: PostLoginResponse = await axios.post(`${BASE_URL}/users/login`, { githubCode });
+  return userData;
 };
 
 export const postLogout = async () => {
