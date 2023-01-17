0626fea8a9e4707ed8d9a6af82899aa25b780972
caseBread <kgu0515@gmail.com>
2023-01-17 20:52:56 +0900

    chore : 불필요한 코드 제거

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,26 +1,4 @@
-import { useState } from 'react';
-import FullModal from '../components/FullModal';
-import ProblemForm from '../components/ProblemForm';
-
 const Main = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  return (
-    <div>
-      Main
-      <button type="button" onClick={() => setShowModal(true)}>
-        button
-      </button>
-      {showModal && (
-        <FullModal close={handleCloseModal}>
-          <ProblemForm close={handleCloseModal} />
-        </FullModal>
-      )}
-    </div>
-  );
+  return <div>main</div>;
 };
 export default Main;
